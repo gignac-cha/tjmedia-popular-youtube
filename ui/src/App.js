cca462c609ebd86512c3a98a7a65cc5ad0be71fb
Jin-Hyoek, Cha <Jin-Hyoek.Cha@outlook.kr>
2019-11-20 04:28:34 +0900

    add ranks api

--- ui/src/App.js
+++ ui/src/App.js
@@ -24,27 +24,162 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      query: {},
+      lastDayOfMonthStart: -1,
+      lastDayOfMonthEnd: -1,
+      ranks: [],
       apiKey: '',
     };
   }
   componentDidMount() {
     this.getApiKey();
+    this.getRanksCached();
   }
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col col-12">
+          <div className="col col-3 p-0">
+            <select className="form-control" name="t" value={this.state.query.t} onChange={this.onChangeQuery}>
+              <option value={1}>한국</option>
+              <option value={2}>외국</option>
+              <option value={3}>일본</option>
+            </select>
+          </div>
+          <div className="col col-1 p-0">
+            <input className="form-control" type="number"
+              min={2017} max={2019} step={1} name="sy"
+              ref={e => this.sy = e} onChange={this.onChangeQuery} />
+          </div>
+          <div className="col col-1 p-0">
+            <input className="form-control" type="number"
+              min={1} max={12} step={1} name="sm"
+              ref={e => this.sm = e} onChange={this.onChangeQuery} />
+          </div>
+          <div className="col col-1 p-0">
+            <input className="form-control" type="number"
+              min={1} max={this.state.lastDayOfMonthStart} step={1} name="sd"
+              ref={e => this.sd = e} onChange={this.onChangeQuery} />
+          </div>
+          <div className="col col-1 p-0">
+            <input className="form-control" type="number"
+              min={2017} max={2019} step={1} name="ey"
+              ref={e => this.ey = e} onChange={this.onChangeQuery} />
+          </div>
+          <div className="col col-1 p-0">
+            <input className="form-control" type="number"
+              min={1} max={12} step={1} name="em"
+              ref={e => this.em = e} onChange={this.onChangeQuery} />
+          </div>
+          <div className="col col-1 p-0">
+            <input className="form-control" type="number"
+              min={1} max={this.state.lastDayOfMonthEnd} step={1} name="ed"
+              ref={e => this.ed = e} onChange={this.onChangeQuery} />
+          </div>
+          <div className="col col-3 p-0">
+            <button className="btn btn-secondary form-control" onClick={this.onClickApply}>Apply</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col col-12 p-0">
+            <table className="table table-sm table-hover table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Rank</th>
+                  <th scope="col">Number</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Artist</th>
+                </tr>
+              </thead>
+              <tbody>{this.state.ranks.map(this.renderRanks)}</tbody>
+            </table>
           </div>
         </div>
       </div>
     );
   }
 
+  renderRanks = (rank, i) => {
+    const ellipsisStyle = { width: 400, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' };
+    return (
+      <tr key={i}>
+        <th scope="row">{rank.rank}</th>
+        <td>{rank.number}</td>
+        <td>
+          <div style={ellipsisStyle}>
+            {rank.title}
+          </div>
+        </td>
+        <td>{rank.artist}</td>
+      </tr>
+    );
+  }
+
+  onChangeQuery = e => {
+    const { name, value } = e.currentTarget;
+    const { query } = this.state;
+    query[name] = value;
+
+    if (name === 'sm') {
+      const lastDayOfMonthStart = moment({ month: value - 1 }).endOf('month').date();
+      if (this.state.lastDayOfMonthStart > lastDayOfMonthStart) {
+        query.sd = lastDayOfMonthStart;
+        this.sd.value = lastDayOfMonthStart;
+      }
+      this.setState({ lastDayOfMonthStart });
+    } else if (name === 'em') {
+      const lastDayOfMonthEnd = moment({ month: value - 1 }).endOf('month').date();
+      if (this.state.lastDayOfMonthEnd > lastDayOfMonthEnd) {
+        query.ed = lastDayOfMonthEnd;
+        this.ed.value = lastDayOfMonthEnd;
+      }
+      this.setState({ lastDayOfMonthEnd });
+    }
+    this.setState({ query });
+  }
+  onClickApply = async e => {
+    this.getRanks();
+  }
+
   getApiKey = async () => {
     const { data } = await axios.get('/api/v1/api-key');
     const { apiKey } = data;
     this.setState({ apiKey });
+  }
+  getRanksCached = async () => {
+    const today = moment();
+    const firstDayOfMonth = moment(today).date(1);
+    const [ t, sy, sm, sd, ey, em, ed ] = [ 1, firstDayOfMonth.year(), firstDayOfMonth.month() + 1, firstDayOfMonth.date(), today.year(), today.month() + 1, today.date() ];
+    this.sy.value = sy;
+    this.sm.value = sm;
+    this.sd.value = sd;
+    this.ey.value = ey;
+    this.em.value = em;
+    this.ed.value = ed;
+    const query = { t, sy, sm, sd, ey, em, ed };
+    const lastDayOfMonthStart = firstDayOfMonth.endOf('month').date();
+    const lastDayOfMonthEnd = firstDayOfMonth.endOf('month').date();
+    this.setState({ query, lastDayOfMonthStart, lastDayOfMonthEnd });
+    const params = query;
+    const { data } = await axios.get('/api/v1/ranks/cached', { params });
+    const { error, message, ranks } = data;
+    if (error) {
+      console.error(message)
+    } else {
+      this.setState({ ranks });
+    }
+  }
+  getRanks = async () => {
+    const { t, sy, sm, sd, ey, em, ed } = this.state.query;
+    const params = { t, sy, sm, sd, ey, em, ed };
+    const response = await axios.get('/api/v1/ranks', { params });
+    const { data } = response;
+    const { error, message, ranks } = data;
+    if (error) {
+      console.error(message)
+    } else {
+      this.setState({ ranks });
+    }
   }
 }
 
--- web/__init__.py
+++ web/__init__.py
@@ -18,6 +18,82 @@ server = flask.Flask(__name__, static_url_path='')
 def index():
   return server.send_static_file("index.html")
 
+cache = {
+  "ranks": {},
+}
+
+def get_ranks(strType, SYY, SMM, SDD, EYY, EMM, EDD):
+  ranks = []
+
+  scheme = "http"
+  netloc = "www.tjmedia.co.kr"
+  path = "/tjsong/song_monthPopular.asp"
+  params = None
+  queries = {
+    "strType": strType,
+    "SYY": SYY, "SMM": SMM, "SDD": SDD,
+    "EYY": EYY, "EMM": EMM, "EDD": EDD,
+  }
+  query = urllib.parse.urlencode(queries)
+  fragment = None
+  url = urllib.parse.urlunparse((scheme, netloc, path, params, query, fragment))
+
+  response = requests.get(url)
+  if response.ok:
+    root = lxml.etree.HTML(response.content)
+    for tr in root.cssselect('#BoardType1 table.board_type1 tr'):
+      tds = tr.xpath('./td')
+      if len(tds) == 4:
+        rank, number, title, artist = (td.text.encode("raw_unicode_escape").decode() for td in tds)
+        ranks.append({
+          "rank": rank,
+          "number": number,
+          "title": title,
+          "artist": artist,
+        })
+  return ranks
+
+@server.route("/api/v1/ranks", methods=[ "GET" ])
+def _api_get_ranks():
+  args = flask.request.args
+  if any(key not in args.keys() for key in ("t", "sy", "sm", "sd", "ey", "em", "ed")):
+    return flask.jsonify(error=True, message="Invalid parameters: 't', 'sy', 'sm', 'sd', 'ey', 'em', 'ed'")
+
+  type_ = args["t"]
+  start_year = args["sy"]
+  start_month = args["sm"]
+  start_day = args["sd"]
+  end_year = args["ey"]
+  end_month = args["em"]
+  end_day = args["ed"]
+
+  start = '-'.join((start_year, start_month, start_day))
+  end = '-'.join((end_year, end_month, end_day))
+  key = '~'.join((start, end))
+  ranks = get_ranks(type_, start_year, start_month, start_day, end_year, end_month, end_day)
+  cache["ranks"][key] = ranks
+  return flask.jsonify(error=False, ranks=ranks)
+
+@server.route("/api/v1/ranks/cached", methods=[ "GET" ])
+def _api_get_ranks_cached():
+  args = flask.request.args
+  if any(key not in args.keys() for key in ("t", "sy", "sm", "sd", "ey", "em", "ed")):
+    return flask.jsonify(error=True, message="Invalid parameters: 't', 'sy', 'sm', 'sd', 'ey', 'em', 'ed'")
+
+  type_ = args["t"]
+  start_year = args["sy"]
+  start_month = args["sm"]
+  start_day = args["sd"]
+  end_year = args["ey"]
+  end_month = args["em"]
+  end_day = args["ed"]
+
+  start = '-'.join((start_year, start_month, start_day))
+  end = '-'.join((end_year, end_month, end_day))
+  key = '~'.join((start, end))
+  if key not in cache["ranks"].keys() or len(cache["ranks"][key]) <= 0:
+    cache["ranks"][key] = get_ranks(type_, start_year, start_month, start_day, end_year, end_month, end_day)
+  return flask.jsonify(error=False, ranks=cache["ranks"][key])
 @server.route("/api/v1/api-key")
 def _api_get_api_key():
   apiKey = os.getenv("API_KEY")
