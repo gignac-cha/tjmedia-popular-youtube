da065dfc092ad2d5cc7a6b8f7c2aff9f32625b2f
Jin-Hyoek, Cha <Jin-Hyoek.Cha@outlook.kr>
2019-11-20 04:19:08 +0900

    add api-key api

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import classnames from 'classnames';
+
+import { AllHtmlEntities } from 'html-entities';
+
+// import $ from 'jquery';
+import _ from 'lodash';
+import moment from 'moment';
+import axios from 'axios';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Add all icons to the library so you can use it in your page
+library.add(fas, far, fab);
+
+const allHtmlEntities = new AllHtmlEntities();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiKey: '',
+    };
+  }
+  componentDidMount() {
+    this.getApiKey();
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col col-12">
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  getApiKey = async () => {
+    const { data } = await axios.get('/api/v1/api-key');
+    const { apiKey } = data;
+    this.setState({ apiKey });
+  }
 }
 
 export default App;
--- web/__init__.py
+++ web/__init__.py
@@ -2,11 +2,26 @@
 
 import flask
 
+import os
+import datetime
+import json
+import urllib
+
+import requests
+import lxml.etree
+
+import pytz
+
 server = flask.Flask(__name__, static_url_path='')
 
 @server.route('/')
 def index():
   return server.send_static_file("index.html")
+
+@server.route("/api/v1/api-key")
+def _api_get_api_key():
+  apiKey = os.getenv("API_KEY")
+  return flask.jsonify(apiKey=apiKey)
 
 if __name__ == "__main__":
   server.run()
