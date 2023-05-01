cbc0ca68f429e03c86d824e5c72b9f43989fa1b4
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2023-05-02 04:09:15 +0900

    fix default values with local storage

--- ui/src/contexts/main.ts
+++ ui/src/contexts/main.ts
@@ -12,29 +12,14 @@ export type MainContextValues = MainContextProperties & MainContextStates;
 
 const doNothing = () => {};
 
-const getType = (): Type => {
-  switch (localStorage.getItem('type')) {
-    default:
-    case '1':
-      return '1';
-    case '2':
-      return '2';
-    case '3':
-      return '3';
-  }
-};
-
-const type: Type = getType();
 const today: Dayjs = dayjs();
-const start: Dayjs = dayjs(localStorage.getItem('start') ?? today.subtract(1, 'month').format('YYYY-MM'));
-const end: Dayjs = dayjs(localStorage.getItem('end') ?? today.format('YYYY-MM'));
 
 export const defaultValues: MainContextValues = {
-  type,
+  type: '1',
   setType: doNothing,
-  start,
+  start: dayjs().subtract(1, 'month'),
   setStart: doNothing,
-  end,
+  end: dayjs(),
   setEnd: doNothing,
   isLoading: false,
   setLoading: doNothing,
