e4f87c34b67f659924f3cc75d067020450b45846
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2023-05-02 03:29:02 +0900

    add local storage cache

--- ui/src/contexts/main.ts
+++ ui/src/contexts/main.ts
@@ -1,8 +1,8 @@
 import { createContext, useContext } from 'react';
 
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
-import { ControlsProperties, ControlsState } from '../components/controls';
+import { ControlsProperties, ControlsState, Type } from '../components/controls';
 import { ListProperties, ListState } from '../components/list';
 import { MainProperties, MainState } from '../components/main';
 
@@ -12,12 +12,29 @@ export type MainContextValues = MainContextProperties & MainContextStates;
 
 const doNothing = () => {};
 
+const getType = (): Type => {
+  switch (localStorage.getItem('type')) {
+    default:
+    case '1':
+      return '1';
+    case '2':
+      return '2';
+    case '3':
+      return '3';
+  }
+};
+
+const type: Type = getType();
+const today: Dayjs = dayjs();
+const start: Dayjs = dayjs(localStorage.getItem('start') ?? today.subtract(1, 'month').format('YYYY-MM'));
+const end: Dayjs = dayjs(localStorage.getItem('end') ?? today.format('YYYY-MM'));
+
 export const defaultValues: MainContextValues = {
-  type: '1',
+  type,
   setType: doNothing,
-  start: dayjs().subtract(1, 'month'),
+  start,
   setStart: doNothing,
-  end: dayjs(),
+  end,
   setEnd: doNothing,
   isLoading: false,
   setLoading: doNothing,
