87450efa3db3c84262d0c4ed70a4f5cd96dde413
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2024-02-18 16:39:01 +0900

    split request function

--- ui/src/utilities/youtube.ts
+++ ui/src/utilities/youtube.ts
@@ -1,47 +1,52 @@
 import { parseDOM } from './htmlParser';
 
-export const getVideoList = async (item: MusicItem): Promise<VideoItem[]> => {
-  const url = new URL(import.meta.env.VITE_YOUTUBE_API_URL);
-  url.searchParams.set('search_query', `${item.title} ${item.artist}`);
-  const response: Response = await fetch(url);
-  const html: string = await response.text();
+const getVideoItems = function* (videoRenderer: YtVideoRenderer) {
+  const videoId: number = videoRenderer.videoId;
+  const { width, height }: YtThumbnail =
+    videoRenderer.thumbnail.thumbnails.reduce(
+      (previousValue: YtThumbnail, currentValue: YtThumbnail) => {
+        const x = (previousValue.width ** 2 + previousValue.height ** 2) ** 0.5;
+        const y = (currentValue.width ** 2 + currentValue.height ** 2) ** 0.5;
+        return x > y ? previousValue : currentValue;
+      },
+    );
+  for (const r of videoRenderer.title.runs) {
+    const title: string = r.text;
+    yield { videoId, title, width, height };
+  }
+};
+const parseVideoList = (html: string) => {
   const newDocument: Document = parseDOM(html);
   const scripts: HTMLScriptElement[] = Array.from(
-    newDocument.querySelectorAll<HTMLScriptElement>('script')
+    newDocument.querySelectorAll<HTMLScriptElement>('script'),
   );
   const startKey = 'var ytInitialData = ';
   const [script]: HTMLScriptElement[] = scripts.filter(
     (script: HTMLScriptElement) =>
-      (script.textContent ?? '').startsWith(startKey)
+      (script.textContent ?? '').startsWith(startKey),
   );
   const ytInitialData: YtInitialData = JSON.parse(
-    (script.textContent ?? '').slice(startKey.length, -1)
+    (script.textContent ?? '').slice(startKey.length, -1),
   );
   const items: VideoItem[] = [];
-  for (const c1 of ytInitialData.contents.twoColumnSearchResultsRenderer
-    .primaryContents.sectionListRenderer.contents) {
-    if (c1.itemSectionRenderer) {
-      for (const c2 of c1.itemSectionRenderer.contents) {
-        if (c2.videoRenderer) {
-          const videoId: number = c2.videoRenderer.videoId;
-          const thumbnail: YtThumbnail =
-            c2.videoRenderer.thumbnail.thumbnails.reduce(
-              (previousValue: YtThumbnail, currentValue: YtThumbnail) => {
-                const x =
-                  (previousValue.width ** 2 + previousValue.height ** 2) ** 0.5;
-                const y =
-                  (currentValue.width ** 2 + currentValue.height ** 2) ** 0.5;
-                return x > y ? previousValue : currentValue;
-              }
-            );
-          for (const r of c2.videoRenderer.title.runs) {
-            const title: string = r.text;
-            const { width, height }: YtThumbnail = thumbnail;
-            items.push({ videoId, title, width, height });
-          }
+  for (const { itemSectionRenderer } of ytInitialData.contents
+    .twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer
+    .contents) {
+    if (itemSectionRenderer) {
+      for (const { videoRenderer } of itemSectionRenderer.contents) {
+        if (videoRenderer) {
+          items.push(...getVideoItems(videoRenderer));
         }
       }
     }
   }
   return items;
+};
+
+export const getVideoList = async (item: MusicItem): Promise<VideoItem[]> => {
+  const url = new URL(import.meta.env.VITE_YOUTUBE_API_URL);
+  url.searchParams.set('search_query', `${item.title} ${item.artist}`);
+  const response: Response = await fetch(url);
+  const html: string = await response.text();
+  return parseVideoList(html);
 };
