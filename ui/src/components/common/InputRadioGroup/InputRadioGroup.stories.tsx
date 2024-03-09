1e54bab02fa5f556db19d2e8b6bbf5fdfc31c8a3
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2024-03-09 23:49:30 +0900

    fix stories

--- ui/src/components/common/InputRadioGroup/InputRadioGroup.stories.tsx
+++ ui/src/components/common/InputRadioGroup/InputRadioGroup.stories.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { Meta, StoryObj } from '@storybook/react';
 import { InputRadioGroup } from './InputRadioGroup';
 
@@ -5,14 +6,21 @@ export default {
   component: InputRadioGroup,
 } satisfies Meta;
 
+const Section = styled.section`
+  margin: auto;
+  width: 40rem;
+`;
+
 export const Default: StoryObj<typeof InputRadioGroup> = {};
 
 export const WithOptions: StoryObj<typeof InputRadioGroup> = {
   render: () => (
-    <InputRadioGroup name="type" defaultValue={'2'}>
-      <InputRadioGroup.Option value={'1'}>가요</InputRadioGroup.Option>
-      <InputRadioGroup.Option value={'2'}>POP</InputRadioGroup.Option>
-      <InputRadioGroup.Option value={'3'}>JPOP</InputRadioGroup.Option>
-    </InputRadioGroup>
+    <Section>
+      <InputRadioGroup name="type" defaultValue={'2'}>
+        <InputRadioGroup.Option value={'1'}>가요</InputRadioGroup.Option>
+        <InputRadioGroup.Option value={'2'}>POP</InputRadioGroup.Option>
+        <InputRadioGroup.Option value={'3'}>JPOP</InputRadioGroup.Option>
+      </InputRadioGroup>
+    </Section>
   ),
 };
